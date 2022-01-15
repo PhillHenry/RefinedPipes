8e979042bbad132f0e22854756424c550ac7c2f7
Phillip Henry <PhillHenry@gmail.com>
2022-01-15 10:54:53 +0000

    Demo.

--- src/main/scala/uk/co/odinconsultants/refinedpipes/data/RefinedPipeExample.scala
+++ src/main/scala/uk/co/odinconsultants/refinedpipes/data/RefinedPipeExample.scala
@@ -15,11 +15,13 @@ object RefinedPipeExample {
     if (age < 40) 1 else 0
 
   val One: BinaryInt = 1
+  val Zero: BinaryInt = 0
 
-  def doModel(x: BinaryInt): Boolean = x == One
+  def doModel(x: BinaryInt): BinaryInt = if (x == One) Zero else One
 
   def main(args: Array[String]): Unit = {
     println(s"Old? ${toBinaryInt(39)}")
+//    println(s"Old? ${toBinaryInt(toBinaryInt(One))}") // doesn't compile
     println(s"pipeline: ${doModel(toBinaryInt(39))}")
 //    println(s"does not compile: ${doModel(39)}")
   }
